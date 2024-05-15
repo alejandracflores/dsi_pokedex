9857289573eea65253dbd106d353c0b8cf7f093f
Alejandra María Cruz Flores <114045083+alejandracflores@users.noreply.github.com>
2024-05-15 12:19:38 -0600

    Actividad 16. Conexión a la Base de Datos

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
+const db = require("./config/database.js");
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.set('view engine', 'ejs');
+//app.set('views', path.join(__dirname, 'views'));
+//app.use(express.static(path.json(__dirname, 'public')));
+
 app.get("/", (req, res, next) => {
-    console.log(req);
-    res.send("Hello world!");
+    res.render("index");
 });
 
-app.get("/pokemon", (req, res, next) => {
-    res.json({ nombre: "Alejandra", edad: "19" })
+app.get("/pokemon", async (req, res, next) => {
+    const pokemon = await db.query("SELECT * FROM pokemon;");
+    console.log(pokemon);
+    res.json(pokemon);
+});
+
+app.post("/pokemon", async (req, res, next) => {
+    console.log(req.body);
+    res.json(req.body);
 });
 
 app.listen(3000, () => {
